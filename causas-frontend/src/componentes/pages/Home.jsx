8393f29555d8eae263139bd6d9556e862e2f4248
jose-carvalho12 <jtnco@iscte-iul.pt>
2026-05-21 21:06:31 +0100

    Add reusable destaque card component

--- causas-frontend/src/componentes/pages/Home.jsx
+++ causas-frontend/src/componentes/pages/Home.jsx
@@ -1,14 +1,22 @@
 import App from "../../App.jsx";
 import HeroCard from "../homepage/HeroCard.jsx";
+import DestaqueCard from "../cards/DestaqueCard.jsx";
+import homeContainerBckg from "../../imagens/home_container_bckg.png";
 
-function Home(){
+function Home() {
+  return (
+    <div className="page">
+      <HeroCard />
 
-    return(
-        <>
-            <div className="page">
-              <HeroCard/>
-            </div>
-        </>
-    );
+      <DestaqueCard
+        titulo="Causas em destaque"
+        descricao="Descobre causas que precisam de apoio e participa na mudança!"
+        imagem={homeContainerBckg}
+        textoBotao="Ver detalhes"
+        link="/causas"
+      />
+    </div>
+  );
 }
-export default Home
+
+export default Home;
